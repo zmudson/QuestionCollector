47e806becd6980b17686ecbb2eedd37c866b1d77
Mixss <michalziemiec@wp.pl>
2021-11-07 22:08:33 +0100

    Added Commands, Question Converters and addded Post-handling controller

--- src/main/java/com/restmate/questioncollector/controllers/IndexController.java
+++ src/main/java/com/restmate/questioncollector/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.restmate.questioncollector.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"","/","index","index.html","/homepage"})
-    public String index(){
+    public String index(Model model){
+
         return "index";
     }
 }
