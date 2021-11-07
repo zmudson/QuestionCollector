363c509e3b3d1bad5c9ee560054ce02370ec89f1
Dominik Zmuda-Trzebiatowski <dominik.zmuda_trzebiatowski@wp.pl>
2021-11-08 00:02:55 +0100

    add repositories, change category description to unique, closes #5

---
+++ src/main/java/com/restmate/questioncollector/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.restmate.questioncollector.repositories;
+
+import com.restmate.questioncollector.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription();
+}
